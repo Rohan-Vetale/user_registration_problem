1211ccdc4b5231d2162cd098289a5b38c6a47e41
rohan vetale <vetalerohan@gmail.com>
2024-01-16 15:28:44 +0530

    [Rohan] Add: Program to check the email valid or not

--- user_reg.py
+++ user_reg.py
@@ -23,7 +23,7 @@ def first_name_check():
     return result
 
 def last_name_check():
-    #UC1
+    #UC2
     user_input = input("Enter Last name starts with Cap and has min. 3 characters -> ")
     pattern = r'^[A-Z].+'
     result = False
@@ -31,13 +31,25 @@ def last_name_check():
         result = re.match(pattern,user_input)
     return result
 
+def email_valid():
+    #UC3
+    user_input = input("Enter your valid email address completely -> ")
+    pattern = r'^[\w.-]+@([\w-]+\.[\w.-]+)$'
+    result = False
+    if len(user_input) > 2:
+        result = re.match(pattern,user_input)
+    return result
 
 result1 = first_name_check()
 result2 = last_name_check()
+result3 = email_valid()
 if result1 :
     print('User has entered the first name correctly')
     
 if result2 :
     print('User has entered the last name correctly')
+    
+if result3 :
+    print('User has entered the email correctly')
     
 
